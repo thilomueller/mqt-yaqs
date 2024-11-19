c6b1db40d87d3fb3fb3cd0f8e066d041b0865e02
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2024-11-19 18:33:02 +0100

    created SimulationParams class

--- src/yaqs/data_structures/simulation_parameters.py
+++ src/yaqs/data_structures/simulation_parameters.py
@@ -1,5 +1,6 @@
 class SimulationParams:
-    def __init__(self,  T: float, dt: float, max_bond_dim: int, N: int):
+    def __init__(self, measurements: dict[str, int], T: float, dt: float, max_bond_dim: int, N: int):
+        self.measurements = measurements
         self.T = T
         self.dt = dt
         self.max_bond_dim = max_bond_dim
