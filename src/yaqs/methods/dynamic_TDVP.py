e82007c199f45c80dfb861d2bb2910e80518fe15
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2024-11-19 18:32:48 +0100

    added <= rather than <

--- src/yaqs/methods/dynamic_TDVP.py
+++ src/yaqs/methods/dynamic_TDVP.py
@@ -3,16 +3,16 @@ if TYPE_CHECKING:
     from yaqs.data_structures.MPO import MPO
     from yaqs.data_structures.MPS import MPS
 
-from yaqs.methods.TDVP_Mendl import single_site_TDVP, two_site_TDVP
+from yaqs.methods.TDVP import single_site_TDVP, two_site_TDVP
 
 
 def dynamic_TDVP(state: 'MPS', H: 'MPO', dt: float, max_bond_dim: int):
     current_max_bond_dim = state.write_max_bond_dim()
 
     # single_site_TDVP(state, H, dt, numsteps=1)
-    if current_max_bond_dim < max_bond_dim:
+    if current_max_bond_dim <= max_bond_dim:
         # Perform 2TDVP
-        two_site_TDVP(state, H, dt, numsteps=1, tol_split=1e-12)
+        two_site_TDVP(state, H, dt, numsteps=1, tol_split=1e-6)
     else:
         # Perform 1TDVP
         single_site_TDVP(state, H, dt, numsteps=1)
