64b50ab06f654a1634b4ddff11b10101e03a61e3
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2024-12-16 03:38:04 +0100

    added decompose theta, new indices not included

--- src/yaqs/circuits/equivalence_checking/check_equivalence.py
+++ src/yaqs/circuits/equivalence_checking/check_equivalence.py
@@ -1,9 +1,52 @@
 from qiskit.converters import dag_to_circuit, circuit_to_dag
 import numpy as np
+import opt_einsum as oe
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from yaqs.general.data_structures.MPO import MPO
+
+
+def decompose_theta(theta, threshold):
+    dims = theta.shape
+    theta = np.transpose(theta, (0, 3, 2, 1, 4, 5))
+    theta_matrix = np.reshape(theta, (dims[0]*dims[1]*dims[2], dims[3]*dims[4]*dims[5]))
+
+    # SVD with truncation
+    U, S_list, V = np.linalg.svd(theta_matrix, full_matrices=False)
+    S_list = S_list[S_list > threshold]
+    U = U[:, 0:len(S_list)]
+    V = V[0:len(S_list), :]
+
+    # Create site tensors
+    U = np.reshape(U, (dims[0], dims[1], dims[2], len(S_list)))
+    U = np.transpose(U, (0, 2, 1, 3))
+    M = np.diag(S_list) @ V
+    M = np.reshape(M, (len(S_list), dims[3], dims[4], dims[5]))
+    M = np.transpose(M, (1, 0, 2, 3))
+
+    return U, M
+
+
+def update_MPO(MPO, dag1, dag2, qubits, threshold):
+    n = qubits[0]
+
+    # print("UPDATE", n, n+1)
+    # TODO: Update to new indices
+    theta = oe.contract('ijkl, mlno->imjkno', MPO.tensors[n], MPO.tensors[n+1])
+    theta = apply_causal_cone(theta, dag1, qubits)
+    theta = apply_causal_cone(theta, dag2, qubits, conjugate=True)
+    MPO.tensors[n], MPO.tensors[n+1] = decompose_theta(theta, threshold)
+
+
+def apply_layer(MPO, circuit1_dag, circuit2_dag, first_iterator, second_iterator, threshold):
+    for n in first_iterator:
+        # print(n, n+1)
+        update_MPO(MPO, circuit1_dag, circuit2_dag, [n, n+1], threshold)
+
+    for n in second_iterator:
+        # print(n, n+1)
+        update_MPO(MPO, circuit1_dag, circuit2_dag, [n, n+1], threshold)
 
 
 def check_longest_gate(DAG_circuit):
@@ -64,13 +107,13 @@ def iterate(mpo: MPO, dag1, dag2, threshold):
         largest_distance2  = check_longest_gate(dag2)
 
         if largest_distance1 in [1, 2] and largest_distance2 in [1, 2]:
-            mpo = apply_layer(mpo, dag1, dag2, first_iterator, second_iterator, threshold)
+            apply_layer(mpo, dag1, dag2, first_iterator, second_iterator, threshold)
         else:
             if largest_distance1 >= largest_distance2:
                 conjugate = False
             else:
                 conjugate = True
-            mpo = apply_long_range_layer(mpo, dag1, dag2, conjugate, threshold)
+            apply_long_range_layer(mpo, dag1, dag2, conjugate, threshold)
     return mpo
 
 
