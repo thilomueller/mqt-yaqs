e9dbdec903a04739a9376ec189bb9298675f81be
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-01-16 14:36:46 +0100

    removed trajectories from WeakSimParams

--- src/yaqs/general/data_structures/simulation_parameters.py
+++ src/yaqs/general/data_structures/simulation_parameters.py
@@ -38,12 +38,10 @@ class PhysicsSimParams:
 
 
 class WeakSimParams:
-    def __init__(self, shots: int, N: int=1000, max_bond_dim: int=2, threshold: float=1e-6):
-        # self.observables = observables
+    def __init__(self, shots: int, max_bond_dim: int=2, threshold: float=1e-6):
         self.avg_prob_dist = None
-        self.prob_dists = N*[None]
+        self.prob_dists = shots*[None]
         self.shots = shots
-        self.N = N
         self.max_bond_dim = max_bond_dim
         self.threshold = threshold
 
