01aa24e4cf36e319d57c8434af354d7eeb67b2c6
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-01-17 11:32:39 +0100

    trash commit

--- examples/3_Shots_Simulator.py
+++ examples/3_Shots_Simulator.py
@@ -10,7 +10,7 @@ from yaqs.general.data_structures.simulation_parameters import Observable, WeakS
 from yaqs.circuits.simulation import simulator
 
 # Define the circuit
-num_qubits = 10
+num_qubits = 5
 depth = num_qubits
 circuit = qiskit.circuit.QuantumCircuit(num_qubits)
 
@@ -19,34 +19,24 @@ twolocal = TwoLocal(num_qubits, ['rx'], ['rzz'], entanglement='linear', reps=dep
 num_pars = len(twolocal.parameters)
 values = np.random.uniform(low=-np.pi, high=np.pi, size=num_pars)
 circuit = copy.deepcopy(twolocal).assign_parameters(values)
-circuit.measure_all()
 
-# L = 10
-# d = 2
-# J = 1
-# g = 0.5
-# H_0 = MPO()
-# H_0.init_Ising(L, d, J, g)
+# Define the noise model
+gamma = 0
+noise_model = NoiseModel(['relaxation'], [gamma])
 
 # Define the initial state
 state = MPS(num_qubits, state='zeros')
 
-# Define the noise model
-# gamma = 0.1
-# noise_model = NoiseModel(['relaxation', 'dephasing'], [gamma, gamma])
-
 # Define the simulation parameters
 shots = 10000
-N = 1
 max_bond_dim = 4
 threshold = 1e-6
-# measurements = [Observable('x', site) for site in range(num_qubits)]
-sim_params = WeakSimParams(shots, N, max_bond_dim, threshold)
+sim_params = WeakSimParams(shots, max_bond_dim, threshold)
 
 if __name__ == "__main__":
-    simulator.run(state, circuit, sim_params)
+    simulator.run(state, circuit, sim_params, noise_model)
 
-    plt.bar(sim_params.prob_dists[0].keys(), sim_params.prob_dists[0].values())
+    plt.bar(sim_params.results.keys(), sim_params.results.values())
     plt.xlabel("Bitstring")
     plt.ylabel("Counts")
     plt.show()
