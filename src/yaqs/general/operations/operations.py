b1a268b666a019403d08a9923f12f5cb15b42c1f
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-01-30 11:02:58 +0100

    removed comments, added assert to check exp value

--- src/yaqs/general/operations/operations.py
+++ src/yaqs/general/operations/operations.py
@@ -27,19 +27,15 @@ def scalar_product(A: 'MPS', B: 'MPS', site: int=-1):
     if site == -1:
         for site in range(A.length):
             tensor = oe.contract('abc, ade->bdce', A_copy.tensors[site], B_copy.tensors[site])
-            # tensor = oe.contract('ijk, abk->iajb', A[site], B[site])
             if site == 0:
                 result = tensor
             else:
                 result = oe.contract('abcd, cdef->abef', result, tensor)
-                # result = oe.contract('ijkl, klcd->ijcd', result, tensor)
         result = np.squeeze(result)
 
     # Used for ignoring other tensors if MPS is in canonical form
     else:
         # Single site operators
-        # print(site, A._check_canonical_form())
-        # assert A._check_canonical_form() == site or A._check_canonical_form == 0 or A._check_canonical_form == A.length-1
         result = oe.contract('ijk, ijk', A_copy.tensors[site], B_copy.tensors[site])
 
     return result
@@ -62,10 +58,11 @@ def local_expval(state: 'MPS', operator: np.ndarray, site: int):
     for i in range(site):
         state.shift_orthogonality_center_right(i)
 
-    # state.set_canonical_form(site)
     temp_state = copy.deepcopy(state)
-    # temp_state.set_canonical_form(site)
     temp_state.tensors[site] = oe.contract('ab, bcd->acd', operator, temp_state.tensors[site])
-    E = scalar_product(temp_state, state, site)
+    E = scalar_product(state, temp_state, site)
+
+    # Expectation values must be real
+    assert E.imag < 1e-16
 
     return E.real
