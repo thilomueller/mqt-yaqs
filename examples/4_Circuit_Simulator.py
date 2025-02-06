fe3f461010e8aab9b89a4e726b9e3baae0ff40fc
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-02-06 15:56:58 +0100

    created general example

--- examples/4_Circuit_Simulator.py
+++ examples/4_Circuit_Simulator.py
@@ -1,7 +1,5 @@
-import copy
 import numpy as np
 import qiskit.circuit
-from qiskit.circuit.library.n_local import TwoLocal
 import matplotlib.pyplot as plt
 
 from yaqs.general.data_structures.networks import MPS
@@ -9,43 +7,49 @@ from yaqs.general.data_structures.noise_model import NoiseModel
 from yaqs.general.data_structures.simulation_parameters import Observable, StrongSimParams
 from yaqs.circuits.simulation import simulator
 
+from yaqs.general.libraries.circuit_library import create_Ising_circuit
+
 # Define the circuit
 num_qubits = 10
-depth = 1
+depth = num_qubits
 circuit = qiskit.circuit.QuantumCircuit(num_qubits)
 
-# Example: Two-Local Circuit
-twolocal = TwoLocal(num_qubits, ['rz'], ['cx'], entanglement='linear', reps=depth).decompose()        
-num_pars = len(twolocal.parameters)
-values = np.random.uniform(low=-np.pi, high=np.pi, size=num_pars)
-circuit = copy.deepcopy(twolocal).assign_parameters(values)
-
-# Define the noise model
-gamma = 0
-noise_model = NoiseModel(['excitation', 'dephasing'], [gamma, gamma])
+model = {'name': 'Ising', 'L': num_qubits, 'J': 1, 'g': 0.5}
+circuit = create_Ising_circuit(model, dt=0.1, timesteps=10)
+circuit.measure_all()
 
 # Define the initial state
 state = MPS(num_qubits, state='zeros')
 
 # Define the simulation parameters
-N = 1000
+N = 100
 max_bond_dim = 4
 threshold = 1e-6
+window_size = 0
 measurements = [Observable('z', site) for site in range(num_qubits)]
-sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold)
 
+gammas = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
 if __name__ == "__main__":
-    simulator.run(state, circuit, sim_params, noise_model)
-    heatmap = []
-    for observable in sim_params.observables:
-        heatmap.append(observable.results)
-        print(observable.results)
+    heatmap = np.empty((num_qubits, len(gammas)))
+
+    for j, gamma in enumerate(gammas):
+        # Define the noise model
+        sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold, window_size)
+        noise_model = NoiseModel(['relaxation'], [gamma])
+        simulator.run(state, circuit, sim_params, noise_model)
+        for i, observable in enumerate(sim_params.observables):
+            heatmap[i, j] = observable.results
 
     fig, ax = plt.subplots(1, 1)
-    im = ax.imshow(heatmap, aspect='auto')
+    im = ax.imshow(heatmap, aspect='auto', vmin=0.5, vmax=1)
     ax.set_ylabel('Site')
+    ax.set_xticks(np.arange(len(gammas)))
+    formatted_gammas = [f"$10^{{{int(np.log10(g))}}}$" for g in gammas]
+    ax.set_xticklabels(formatted_gammas)
 
-    # plt.bar(sim_params.results.keys(), sim_params.results.values())
-    # plt.xlabel("Bitstring")
-    # plt.ylabel("Counts")
+    fig.subplots_adjust(top=0.95, right=0.88)
+    cbar_ax = fig.add_axes([0.9, 0.11, 0.025, 0.8])
+    cbar = fig.colorbar(im, cax=cbar_ax)
+    cbar.ax.set_title('$\\langle Z \\rangle$')
+
     plt.show()
