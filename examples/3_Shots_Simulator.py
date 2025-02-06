4f898d0e44bc1474f43166773ad9683ec0231e82
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-02-06 15:56:49 +0100

    added window option

--- examples/3_Shots_Simulator.py
+++ examples/3_Shots_Simulator.py
@@ -1,37 +1,39 @@
 import copy
-import numpy as np
-import qiskit.circuit
-from qiskit.circuit.library.n_local import TwoLocal
 import matplotlib.pyplot as plt
+import numpy as np
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit.library.n_local import TwoLocal
 
+from yaqs.circuits.simulation import simulator
 from yaqs.general.data_structures.networks import MPS
 from yaqs.general.data_structures.noise_model import NoiseModel
-from yaqs.general.data_structures.simulation_parameters import Observable, WeakSimParams
-from yaqs.circuits.simulation import simulator
+from yaqs.general.data_structures.simulation_parameters import WeakSimParams
+
 
 # Define the circuit
-num_qubits = 5
-depth = num_qubits
-circuit = qiskit.circuit.QuantumCircuit(num_qubits)
+num_qubits = 10
+circuit = QuantumCircuit(num_qubits)
 
 # Example: Two-Local Circuit
-twolocal = TwoLocal(num_qubits, ['rx'], ['rzz'], entanglement='linear', reps=depth).decompose()        
+twolocal = TwoLocal(num_qubits, ['rx'], ['rzz'], entanglement='linear', reps=num_qubits).decompose()        
 num_pars = len(twolocal.parameters)
 values = np.random.uniform(low=-np.pi, high=np.pi, size=num_pars)
 circuit = copy.deepcopy(twolocal).assign_parameters(values)
+circuit.measure_all()
 
 # Define the noise model
-gamma = 1
-noise_model = NoiseModel(['relaxation'], [gamma])
+gamma = 0.1
+noise_model = NoiseModel(['relaxation', 'dephasing'], [gamma, gamma])
 
 # Define the initial state
 state = MPS(num_qubits, state='zeros')
 
 # Define the simulation parameters
-shots = 1000
+shots = 1024
 max_bond_dim = 4
 threshold = 1e-6
-sim_params = WeakSimParams(shots, max_bond_dim, threshold)
+window_size = 0
+sim_params = WeakSimParams(shots, max_bond_dim, threshold, window_size)
 
 if __name__ == "__main__":
     simulator.run(state, circuit, sim_params, noise_model)
