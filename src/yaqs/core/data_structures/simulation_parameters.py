522996c8e5d5260e522afbc8f9c9532e53a70b8e
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-02-21 18:11:35 +0100

    added matrix property to observable for custom observables

--- src/yaqs/core/data_structures/simulation_parameters.py
+++ src/yaqs/core/data_structures/simulation_parameters.py
@@ -4,9 +4,12 @@ from yaqs.core.libraries.gate_library import GateLibrary
 
 class Observable:
     def __init__(self, name: str, site: int, matrix: np.ndarray=None):
-        if name != 'custom':
-            assert getattr(GateLibrary, name)
+        assert getattr(GateLibrary, name)
         assert (matrix is None and name != 'custom') or (matrix is not None and name == 'custom')
+        if name == 'custom':
+            self.matrix = matrix
+        else:
+            self.matrix = None
         self.name = name
         self.site = site
         self.results = None
