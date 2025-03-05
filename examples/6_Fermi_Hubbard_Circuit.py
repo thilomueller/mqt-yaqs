31a43cd7b4ad26fa7e4151b400286cb288f77f3e
thilomueller <thilo.r.mueller@gmail.com>
2025-03-05 19:46:22 +0100

    generate Trotter convergence diagram

--- examples/6_Fermi_Hubbard_Circuit.py
+++ examples/6_Fermi_Hubbard_Circuit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
 import qiskit.circuit
 from qiskit_nature.second_q.hamiltonians import FermiHubbardModel
 from qiskit_nature.second_q.hamiltonians.lattices import LineLattice, SquareLattice, BoundaryCondition
@@ -19,30 +20,51 @@ timesteps = 1
 # Define the circuit
 num_sites = x*y
 num_qubits = 2*num_sites
-num_trotter_steps = 300
-circuit = qiskit.circuit.QuantumCircuit(num_qubits)
+#num_trotter_steps = 10
 
-model = {'name': '2D_Fermi_Hubbard', 'Lx': x, 'Ly': y, 'mu': v, 'u': u, 't': t, 'num_trotter_steps': num_trotter_steps}
-circuit = create_2D_Fermi_Hubbard_circuit(model, dt=dt, timesteps=timesteps)
-U_yaqs = Operator(circuit).to_matrix()
+dt_list = [10**(-x) for x in range(4)]
+trotter_steps_list = [10**x for x in range(4)]
 
-# Qiskit implementation
-square_lattice = SquareLattice(rows=x, cols=y, boundary_condition=BoundaryCondition.OPEN)
-#line_lattice = LineLattice(num_nodes=x, boundary_condition=BoundaryCondition.OPEN)
+for dt in dt_list:
+    error_list = []
+    for num_trotter_steps in trotter_steps_list:
+        circuit = qiskit.circuit.QuantumCircuit(num_qubits)
 
-fh_hamiltonian = FermiHubbardModel(
-    square_lattice.uniform_parameters(
-        uniform_interaction=t,
-        uniform_onsite_potential=v,
-    ),
-    onsite_interaction=u,
-)
+        model = {'name': '2D_Fermi_Hubbard', 'Lx': x, 'Ly': y, 'mu': v, 'u': u, 't': t, 'num_trotter_steps': num_trotter_steps}
+        circuit = create_2D_Fermi_Hubbard_circuit(model, dt=dt, timesteps=timesteps)
+        U_yaqs = Operator(circuit).to_matrix()
 
-mapper = JordanWignerMapper()
-qubit_jw_op = mapper.map(fh_hamiltonian.second_q_op())
-H_qiskit = qubit_jw_op.to_matrix()
-U_qiskit = sp.linalg.expm(-1j*dt*timesteps*H_qiskit)
+        # Qiskit implementation
+        square_lattice = SquareLattice(rows=x, cols=y, boundary_condition=BoundaryCondition.OPEN)
+        #line_lattice = LineLattice(num_nodes=x, boundary_condition=BoundaryCondition.OPEN)
 
-# Calculate error
-error = np.linalg.norm(U_yaqs - U_qiskit, 2)
-print("Trotter error: " + str(error))
+        fh_hamiltonian = FermiHubbardModel(
+            square_lattice.uniform_parameters(
+                uniform_interaction=t,
+                uniform_onsite_potential=v,
+            ),
+            onsite_interaction=u,
+        )
+
+        mapper = JordanWignerMapper()
+        qubit_jw_op = mapper.map(fh_hamiltonian.second_q_op())
+        H_qiskit = qubit_jw_op.to_matrix()
+        U_qiskit = sp.linalg.expm(-1j*dt*timesteps*H_qiskit)
+
+        # Calculate error
+        error = np.linalg.norm(U_yaqs - U_qiskit, 2)
+        error_list.append(error)
+        print("Trotter error: " + str(error))
+
+    print(error_list)
+    label = "$\Delta t = $" + str(dt)
+    plt.loglog(trotter_steps_list, error_list, linestyle='-', marker='s', label=label)
+
+#plt.rcParams['text.usetex'] = True
+plt.xlabel("number of Trotter steps $n$")
+plt.ylabel("error")
+plt.xscale("log", base=10)
+plt.yscale("log",base=10)
+plt.title("Convergence of the Trotter splitting error")
+plt.legend()
+plt.show()
