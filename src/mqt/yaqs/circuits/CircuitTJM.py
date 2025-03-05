16f6a66b3fc833ec6329d3a43ec18bc64e981cc1
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-03-05 18:34:26 +0100

    fixed typing and added window size assert

--- src/mqt/yaqs/circuits/CircuitTJM.py
+++ src/mqt/yaqs/circuits/CircuitTJM.py
@@ -23,8 +23,10 @@ from .utils.dag_utils import convert_dag_to_tensor_algorithm
 
 if TYPE_CHECKING:
     from qiskit.dagcircuit import DAGCircuit, DAGOpNode
+
     from ..core.data_structures.simulation_parameters import StrongSimParams, WeakSimParams
     from ..core.libraries.gate_library import BaseGate
+
 
 def process_layer(dag: DAGCircuit) -> tuple[list[DAGOpNode], list[DAGOpNode], list[DAGOpNode]]:
     # Extract the current layer
@@ -64,7 +66,7 @@ def apply_single_qubit_gate(state: MPS, node: DAGOpNode) -> None:
     state.tensors[gate.sites[0]] = oe.contract("ab, bcd->acd", gate.tensor, state.tensors[gate.sites[0]])
 
 
-def construct_generator_MPO(gate: BaseGate, length: int) -> MPO | int:
+def construct_generator_MPO(gate: BaseGate, length: int) -> tuple[MPO, int, int]:
     tensors = []
 
     first_site = min(gate.sites)
@@ -97,8 +99,11 @@ def construct_generator_MPO(gate: BaseGate, length: int) -> MPO | int:
     return mpo, first_site, last_site
 
 
-def apply_window(state: MPS, mpo: MPO, first_site: int, last_site: int, sim_params: StrongSimParams | WeakSimParams) -> tuple[MPS, MPO, list[int]]:
+def apply_window(
+    state: MPS, mpo: MPO, first_site: int, last_site: int, sim_params: StrongSimParams | WeakSimParams
+) -> tuple[MPS, MPO, list[int]]:
     # Define a window for a local update.
+    assert sim_params.window_size is not None
     window = [first_site - sim_params.window_size, last_site + sim_params.window_size]
     window[0] = max(window[0], 0)
     window[1] = min(window[1], state.length - 1)
