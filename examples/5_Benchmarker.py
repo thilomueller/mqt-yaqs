141530aa724bc3a2ba0d1f26e5c41b8ccc841908
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-03-06 15:27:33 +0100

    removed model in benchmarker

--- examples/5_Benchmarker.py
+++ examples/5_Benchmarker.py
@@ -12,7 +12,8 @@ from mqt.yaqs.core.libraries.circuit_library import create_Ising_circuit
 
 if __name__ == "__main__":
     num_qubits = 10
-    model = {"name": "Ising", "L": num_qubits, "J": 1, "g": 0.5}
-    demo_circuit = create_Ising_circuit(model, dt=0.1, timesteps=10)
+    J = 1
+    g = 0.5
+    demo_circuit = create_Ising_circuit(num_qubits, J, g, dt=0.1, timesteps=10)
     # Run the benchmark on the demo circuit.
     benchmarker.run(demo_circuit, style="dots")
