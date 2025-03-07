76de9270734e6b4eaa4ba8503b2ee32315cd5ad2
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-03-07 21:50:59 +0100

    removed qutip dependency in example

--- examples/1_Noisy_Ising_Model.py
+++ examples/1_Noisy_Ising_Model.py
@@ -8,8 +8,6 @@
 from __future__ import annotations
 
 import matplotlib.pyplot as plt
-import numpy as np
-import qutip as qt
 
 from mqt.yaqs import Simulator
 from mqt.yaqs.core.data_structures.networks import MPO, MPS
@@ -41,66 +39,21 @@ order = 2
 measurements = [Observable("x", site) for site in range(L)]
 sim_params = PhysicsSimParams(measurements, T, dt, sample_timesteps, N, max_bond_dim, threshold, order)
 
-fig, ax = plt.subplots(2, 1)
+fig, ax = plt.subplots(1, 1)
 if __name__ == "__main__":
     # TJM Example #################
     Simulator.run(state, H_0, sim_params, noise_model)
 
     heatmap = [observable.results for observable in sim_params.observables]
 
-    im = ax[0].imshow(heatmap, aspect="auto", extent=[0, T, L, 0], vmin=0, vmax=0.5)
-    ax[0].set_ylabel("Site")
-
-    # Centers site ticks
-    ax[0].set_yticks([x - 0.5 for x in list(range(1, L + 1))], range(1, L + 1))
-    #########################################
-
-    # ######### QuTip Exact Solver ############
-    # Time vector
-    t = np.arange(0, sim_params.T + sim_params.dt, sim_params.dt)
-
-    # Define Pauli matrices
-    sx = qt.sigmax()
-    sy = qt.sigmay()
-    sz = qt.sigmaz()
-
-    # Construct the Ising Hamiltonian
-    H = 0
-    for i in range(L - 1):
-        H += -J * qt.tensor([sz if n == i or n == i + 1 else qt.qeye(2) for n in range(L)])
-    for i in range(L):
-        H += -g * qt.tensor([sx if n == i else qt.qeye(2) for n in range(L)])
-
-    # Construct collapse operators
-
-    # Relaxation operators
-    c_ops = [np.sqrt(gamma) * qt.tensor([qt.destroy(2) if n == i else qt.qeye(2) for n in range(L)]) for i in range(L)]
-
-    # Dephasing operators
-    c_ops.extend(np.sqrt(gamma) * qt.tensor([sz if n == i else qt.qeye(2) for n in range(L)]) for i in range(L))
-
-    # Initial state
-    psi0 = qt.tensor([qt.basis(2, 0) for _ in range(L)])
-
-    # Define measurement operators
-    sx_list = [qt.tensor([sx if n == i else qt.qeye(2) for n in range(L)]) for i in range(L)]
-
-    # Exact Lindblad solution
-    result_lindblad = qt.mesolve(H, psi0, t, c_ops, sx_list, progress_bar=True)
-    heatmap2 = [result_lindblad.expect[site] for site in range(len(sx_list))]
-
-    # Error heatmap
-    heatmap = np.array(heatmap)
-    heatmap2 = np.array(heatmap2)
-    im2 = ax[1].imshow(heatmap2, aspect="auto", extent=[0, T, L, 0], vmin=0, vmax=0.5)
-
-    ax[1].set_yticks([x - 0.5 for x in list(range(1, L + 1))], range(1, L + 1))
+    im = plt.imshow(heatmap, aspect="auto", extent=[0, T, L, 0], vmin=0, vmax=0.5)
+    plt.xlabel("Site")
+    plt.yticks([x - 0.5 for x in list(range(1, L + 1))], range(1, L + 1))
+    plt.ylabel("t")
 
     fig.subplots_adjust(top=0.95, right=0.88)
     cbar_ax = fig.add_axes([0.9, 0.11, 0.025, 0.8])
     cbar = fig.colorbar(im, cax=cbar_ax)
     cbar.ax.set_title("$\\langle X \\rangle$")
 
-    ax[1].set_xlabel("t")
-    ax[1].set_ylabel("Site")
     plt.show()
