ef3c5bfadcfe5528a2bad036cce9721a67e08445
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-03-08 19:07:17 +0100

    moved __future__ import before docstring to satisfy ruff

--- src/mqt/yaqs/__init__.py
+++ src/mqt/yaqs/__init__.py
@@ -4,12 +4,11 @@
 # SPDX-License-Identifier: MIT
 #
 # Licensed under the MIT License
+from __future__ import annotations
 
 """Yet Another Quantum Simulator (YAQS), a part of the Munich Quantum Toolkit (MQT),
 is a package to facilitate simulation for the exploration of noise in quantum systems.
 """
-
-from __future__ import annotations
 
 from ._version import version as __version__
 from ._version import version_tuple as version_info
