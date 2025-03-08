fb9e9234da40da892a22302a30937c9b04c0b354
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-03-08 19:11:47 +0100

    fixed ruff error

--- src/mqt/yaqs/__init__.py
+++ src/mqt/yaqs/__init__.py
@@ -4,11 +4,14 @@
 # SPDX-License-Identifier: MIT
 #
 # Licensed under the MIT License
-from __future__ import annotations
 
-"""Yet Another Quantum Simulator (YAQS), a part of the Munich Quantum Toolkit (MQT),
+"""YAQS init file.
+
+Yet Another Quantum Simulator (YAQS), a part of the Munich Quantum Toolkit (MQT),
 is a package to facilitate simulation for the exploration of noise in quantum systems.
 """
+
+from __future__ import annotations
 
 from ._version import version as __version__
 from ._version import version_tuple as version_info
