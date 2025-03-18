4f6ee9ef016c955e044122bcf6e2bc431462371e
thilomueller <thilo.r.mueller@gmail.com>
2025-03-18 16:08:25 +0100

    changed FH example to calculate total occupation number and use step-wise calculation

--- examples/8_Fermi_Hubbard_site_occupation.py
+++ examples/8_Fermi_Hubbard_site_occupation.py
@@ -10,17 +10,17 @@ from mqt.yaqs.core.libraries.circuit_library import create_2D_Fermi_Hubbard_circ
 from mqt.yaqs import simulator
 
 # Define the model
-t = -1.0        # kinetic hopping
+t = 1.0         # kinetic hopping
 v = 0.5         # chemical potential
-u = 2.0         # onsite interaction
+u = 4.0         # onsite interaction
 Lx, Ly = 2, 2   # lattice dimensions
 
 # Define the circuit
 num_sites = Lx * Ly
 num_qubits = 2*num_sites
 num_trotter_steps = 10
-timesteps = 10
-dt = 1
+timesteps = 20
+dt = 0.1
 total_time = dt * timesteps
 
 # Define the initial state
@@ -33,26 +33,35 @@ threshold = 1e-6
 window_size = 0
 measurements = [Observable('z', site) for site in range(num_qubits)]
 
-#heatmap = np.empty((num_qubits, timesteps))
-heatmap = np.zeros((num_sites, timesteps))
+heatmap = np.zeros((num_qubits, timesteps), dtype='complex')
+#heatmap = np.ones((num_sites, timesteps))
 
 if __name__ == "__main__":
     for timestep in range(timesteps):
         print("Timestep: " + str(timestep))
-        model = {'name': '2D_Fermi_Hubbard', 'Lx': Lx, 'Ly': Ly, 'mu': v, 'u': u, 't': t, 'num_trotter_steps': num_trotter_steps}
-        circuit = create_2D_Fermi_Hubbard_circuit(model, dt=dt, timesteps=timestep)
+        model = {'name': '2D_Fermi_Hubbard', 'Lx': Lx, 'Ly': Ly, 'mu': -v, 'u': u, 't': -t, 'num_trotter_steps': num_trotter_steps}
+        circuit = create_2D_Fermi_Hubbard_circuit(model, dt=dt, timesteps=1)
         circuit.measure_all()
 
-        sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold, window_size)
-        #gamma = 1e-5
-        #noise_model = NoiseModel(['relaxation'], [gamma])
+        sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold, window_size, get_state=True)
+
         simulator.run(state, circuit, sim_params, None)
 
-        for observable in sim_params.observables:
-            index = observable.site // 2
-            print(str(observable.site) + " -> " + str(index))
-            heatmap[index, timestep] += (1/2 * (1 - observable.results.item()))
+        #for observable in sim_params.observables:
+        #    index = observable.site // 2
+        #    print(str(observable.site) + " -> " + str(index))
+        #    print((1/2 * (1 - observable.results.item())))
+        #    #heatmap[index, timestep] += (1/2 * (1 - observable.results.item()))
+        #    heatmap[index, timestep] -= 0.5 * observable.results.item()
 
-    print(heatmap.shape)
+        for observable in sim_params.observables:
+            index = observable.site
+            print(str(observable.site) + " -> " + str(index))
+            print(((observable.results.item())))
+            #heatmap[index, timestep] += (1/2 * (1 - observable.results.item()))
+            heatmap[index, timestep] = 0.5 * (1 - observable.results.item())
+
+        state = sim_params.output_state
+
     plt.plot(heatmap.transpose())
     plt.show()
