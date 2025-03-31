cc65a1ec5406fb9751a692ad086be93d4a175a95
thilomueller <thilo.r.mueller@gmail.com>
2025-03-31 17:32:57 +0200

    set simulation paramteres high enough to get accurate results

--- examples/8_Fermi_Hubbard_site_occupation.py
+++ examples/8_Fermi_Hubbard_site_occupation.py
@@ -20,48 +20,43 @@ num_sites = Lx * Ly
 num_qubits = 2 * num_sites
 num_trotter_steps = 1
 timesteps = 1000
-dt = 0.1
+dt = 0.01
 total_time = dt * timesteps
+print("Total time: ", total_time)
 
 # Define the initial state
-state = MPS(num_qubits, state='wall')
+state = MPS(num_qubits, state='wall', pad=32)
 
 # Define the simulation parameters
 N = 1
-max_bond_dim = 4
+max_bond_dim = 16
 threshold = 1e-6
 window_size = 0
 measurements = [Observable('z', site) for site in range(num_qubits)]
 
-heatmap = np.zeros((num_qubits, timesteps), dtype='complex')
-#heatmap = np.ones((num_sites, timesteps))
+occupations = np.zeros((num_qubits, timesteps), dtype='complex')
 
 if __name__ == "__main__":
     for timestep in range(timesteps):
         print("Timestep: " + str(timestep))
         model = {'name': '2D_Fermi_Hubbard', 'Lx': Lx, 'Ly': Ly, 'mu': -v, 'u': u, 't': -t, 'num_trotter_steps': num_trotter_steps}
         circuit = create_2D_Fermi_Hubbard_circuit(model, dt=dt, timesteps=1)
-        circuit.measure_all()
 
         sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold, window_size, get_state=True)
 
         simulator.run(state, circuit, sim_params, None)
 
-        #for observable in sim_params.observables:
-        #    index = observable.site // 2
-        #    heatmap[index, timestep] -= 0.5 * observable.results.item()
-
         for observable in sim_params.observables:
             index = observable.site
-            heatmap[index, timestep] = 0.5 * (1 - observable.results.item())
+            occupations[index, timestep] = 0.5 * (1 - observable.results.item())
 
-        state = sim_params.output_state
+        state = MPS(num_qubits, sim_params.output_state.tensors, pad=32)
 
 plt.figure(figsize=(10, 5))
 for i in range(num_qubits):
     site = i // 2
     spin = "↑" if i % 2 == 0 else '↓'
-    plt.plot(heatmap[i, :], label=f"Site {site} " + spin)
+    plt.plot(occupations[i, :], label=f"Site {site} " + spin)
 
 plt.xlabel("Time")
 plt.ylabel("Occupation")
