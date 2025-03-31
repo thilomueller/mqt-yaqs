50cacd8b900b19030adc823f2cd879b0ff008603
thilomueller <thilo.r.mueller@gmail.com>
2025-03-31 19:43:14 +0200

    remove unused imports from FH example

--- examples/8_Fermi_Hubbard_site_occupation.py
+++ examples/8_Fermi_Hubbard_site_occupation.py
@@ -1,9 +1,7 @@
 import numpy as np
-import qiskit.circuit
 import matplotlib.pyplot as plt
 
 from mqt.yaqs.core.data_structures.networks import MPS
-from mqt.yaqs.core.data_structures.noise_model import NoiseModel
 from mqt.yaqs.core.data_structures.simulation_parameters import Observable, StrongSimParams
 from mqt.yaqs.core.libraries.circuit_library import create_2D_Fermi_Hubbard_circuit
 
