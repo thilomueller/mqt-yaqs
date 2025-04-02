1d54e2f7594d27466c88e42ecb20398f82c32fe4
thilomueller <thilo.r.mueller@gmail.com>
2025-04-02 16:34:06 +0200

    added QuTiP as comparison

--- src/mqt/yaqs/circuits/reference_implementation/fermi_hubbard_benchmarker.py
+++ src/mqt/yaqs/circuits/reference_implementation/fermi_hubbard_benchmarker.py
@@ -18,11 +18,13 @@ reference implementations.
 from __future__ import annotations
 
 import numpy as np
+import qutip as qt
 import matplotlib.pyplot as plt
 
 from mqt.yaqs.core.data_structures.networks import MPS
 from mqt.yaqs.core.data_structures.simulation_parameters import Observable, StrongSimParams
 from mqt.yaqs.core.libraries.circuit_library import create_2d_fermi_hubbard_circuit
+from mqt.yaqs.circuits.reference_implementation.fermi_hubbard_reference import create_fermi_hubbard_model_qutip
 
 from mqt.yaqs import simulator
 
@@ -33,11 +35,12 @@ def benchmarker() -> None:
     mu = 0.5        # chemical potential
     u = 4.0         # onsite interaction
     Lx, Ly = 2, 2   # lattice dimensions
+    L = Lx * Ly     # total lattice sites
 
     num_sites = Lx * Ly
     num_qubits = 2 * num_sites
     num_trotter_steps = 1
-    timesteps = 1000
+    timesteps = 1
     dt = 0.01
     total_time = dt * timesteps
     print("Total time: ", total_time)
@@ -71,17 +74,24 @@ def benchmarker() -> None:
 
         state = MPS(num_qubits, sim_params.output_state.tensors, pad=32)
 
-    plt.figure(figsize=(10, 5))
-    for i in range(num_qubits):
-        site = i // 2
-        spin = "↑" if i % 2 == 0 else '↓'
-        plt.plot(occupations[i, :], label=f"Site {site} " + spin)
+    state = MPS(num_qubits, state='wall', pad=32)
+    state_yaqs = state.to_vec()
 
-    plt.xlabel("Time")
-    plt.ylabel("Occupation")
-    plt.title("2D Hubbard Model: Time Evolution of Site Occupations (mqt-yaqs)")
-    plt.legend()
-    plt.show()
+    
+    state_list = sum(([qt.basis(2, 0), qt.basis(2, 0)] if x >= L/2 else [qt.basis(2, 1), qt.basis(2, 1)] for x in range(L)), [])    # brick wall state
+    initial_state = qt.tensor(state_list)
+
+    H = create_fermi_hubbard_model_qutip(Lx, Ly, u, -t, mu)
+
+    tlist = np.linspace(0, 10, 100)
+    result = qt.mesolve(H, initial_state, tlist, [], [])
+    print(result.states[0].full())
+
+    #state_qutip = result.states[0].full()
+    state_qutip = initial_state.full()
+
+    error = np.linalg.norm(state_qutip - state_yaqs, 2)
+    print(error)
 
 
 if __name__ == "__main__":
