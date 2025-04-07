24e7a0b83b5200e1e50a071dc3a17a2e105f41b2
thilomueller <thilo.r.mueller@gmail.com>
2025-04-07 16:15:00 +0200

    include varying timestep number and trotter steps in benchmark

--- src/mqt/yaqs/circuits/reference_implementation/fermi_hubbard_benchmarker.py
+++ src/mqt/yaqs/circuits/reference_implementation/fermi_hubbard_benchmarker.py
@@ -40,98 +40,103 @@ def benchmarker() -> None:
 
     num_sites = Lx * Ly
     num_qubits = 2 * num_sites
-    num_trotter_steps = 1
-    timesteps = 1
+
+    timesteps_list = [1, 10, 100]
+    num_trotter_steps_list = [1, 10]
     dt = 0.01
-    total_time = dt * timesteps
-    print("Total time: ", total_time)
 
-    ##################################
-    # Reference simulation
-    ##################################
-    state_list = sum(([qt.basis(2, 0), qt.basis(2, 0)] if x >= L/2 else [qt.basis(2, 1), qt.basis(2, 1)] for x in range(L)), [])    # brick wall state
-    initial_state = qt.tensor(state_list)
+    fig, ax = plt.subplots(len(timesteps_list), len(num_trotter_steps_list))
+    plt.tight_layout()
 
-    H = create_fermi_hubbard_model_qutip(Lx, Ly, u, -t, mu)
+    for index_timesteps, timesteps in enumerate(timesteps_list):
+        for index_trotter, num_trotter_steps in enumerate(num_trotter_steps_list):
+            ##################################
+            # Reference simulation
+            ##################################
+            state_list = sum(([qt.basis(2, 0), qt.basis(2, 0)] if x >= L/2 else [qt.basis(2, 1), qt.basis(2, 1)] for x in range(L)), [])    # brick wall state
+            initial_state = qt.tensor(state_list)
 
-    tlist = np.linspace(0, total_time, timesteps)
-    result = qt.mesolve(H, initial_state, tlist, [], [])
-    occupations_qutip = np.zeros((num_qubits, timesteps), dtype='complex')
+            H = create_fermi_hubbard_model_qutip(Lx, Ly, u, -t, mu)
 
-    for x in range(Lx):
-        for y in range(Ly):
-            i_up = site_index(x, y, '↑', Lx)
-            i_down = site_index(x, y, '↓', Lx)
-            n_up = qt.fcreate(n_sites=2*L, site=i_up) * qt.fdestroy(n_sites=2*L, site=i_up)
-            n_down = qt.fcreate(n_sites=2*L, site=i_down) * qt.fdestroy(n_sites=2*L, site=i_down)
-            occupations_qutip[i_up, :] = qt.expect(n_up, result.states)
-            occupations_qutip[i_down, :] = qt.expect(n_down, result.states)
+            total_time = dt * timesteps
+            print("-------------------------")
+            print("T = " + str(total_time))
+            
+            tlist = np.linspace(0, total_time, timesteps)
+            result = qt.mesolve(H, initial_state, tlist, [], [])
+            occupations_qutip = np.zeros((num_qubits, timesteps), dtype='complex')
 
-    ##################################
-    # YAQS simulation
-    ##################################
+            for x in range(Lx):
+                for y in range(Ly):
+                    i_up = site_index(x, y, '↑', Lx)
+                    i_down = site_index(x, y, '↓', Lx)
+                    n_up = qt.fcreate(n_sites=2*L, site=i_up) * qt.fdestroy(n_sites=2*L, site=i_up)
+                    n_down = qt.fcreate(n_sites=2*L, site=i_down) * qt.fdestroy(n_sites=2*L, site=i_down)
+                    occupations_qutip[i_up, :] = qt.expect(n_up, result.states)
+                    occupations_qutip[i_down, :] = qt.expect(n_down, result.states)
 
-    # Define the simulation parameters
-    N = 1
-    window_size = 0
-    measurements = [Observable('z', site) for site in range(num_qubits)]
+            ##################################
+            # YAQS simulation
+            ##################################
 
-    #max_bond_dim_list = range(2, 2**num_qubits//2+1, 2)
-    #threshold_list = [10**-x for x in range(1, 13)]
+            # Define the simulation parameters
+            N = 1
+            window_size = 0
+            measurements = [Observable('z', site) for site in range(num_qubits)]
 
-    max_bond_dim_list = range(2, 2**3//2+1, 2)
-    threshold_list = [10**-x for x in range(1, 5)]
+            max_bond_dim_list = range(2, 2**num_qubits//2+1, 2)
+            threshold_list = [10**-x for x in range(1, 13)]
 
-    heatmap = np.empty((len(max_bond_dim_list), len(threshold_list)))
+            heatmap = np.empty((len(max_bond_dim_list), len(threshold_list)))
 
-    for i, max_bond_dim in enumerate(max_bond_dim_list):
-        for j, threshold in enumerate(threshold_list):
-            # Define the initial state
-            state = MPS(num_qubits, state='wall', pad=32)
+            for index_dim, max_bond_dim in enumerate(max_bond_dim_list):
+                for index_threshold, threshold in enumerate(threshold_list):
+                    # Define the initial state
+                    state = MPS(num_qubits, state='wall', pad=32)
 
-            # Run the simulation
-            occupations_yaqs = np.zeros((num_qubits, timesteps), dtype='complex')
+                    # Run the simulation
+                    occupations_yaqs = np.zeros((num_qubits, timesteps), dtype='complex')
 
-            for timestep in range(timesteps):
-                print("Timestep: " + str(timestep))
-                circuit = create_2d_fermi_hubbard_circuit(Lx=Lx, Ly=Ly, u=u, t=t, mu=mu,
-                                                        num_trotter_steps=num_trotter_steps,
-                                                        dt=dt, timesteps=1)
+                    for timestep in range(timesteps):
+                        print("Timestep: " + str(timestep))
+                        circuit = create_2d_fermi_hubbard_circuit(Lx=Lx, Ly=Ly, u=u, t=t, mu=mu,
+                                                                num_trotter_steps=num_trotter_steps,
+                                                                dt=dt, timesteps=1)
 
-                sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold, window_size, get_state=True)
+                        sim_params = StrongSimParams(measurements, N, max_bond_dim, threshold, window_size, get_state=True)
 
-                simulator.run(state, circuit, sim_params, None)
+                        simulator.run(state, circuit, sim_params, None)
 
-                for observable in sim_params.observables:
-                    index = observable.site
-                    occupations_yaqs[index, timestep] = 0.5 * (1 - observable.results.item())
+                        for observable in sim_params.observables:
+                            index = observable.site
+                            occupations_yaqs[index, timestep] = 0.5 * (1 - observable.results.item())
 
-                state = MPS(num_qubits, sim_params.output_state.tensors, pad=32)
+                        state = MPS(num_qubits, sim_params.output_state.tensors, pad=32)
 
-            # Calculate error
-            error = np.linalg.norm(occupations_yaqs[:,-1][::-1] - occupations_qutip[:,-1], 2)
-            print("max bond dim: ", max_bond_dim, "\t threshold: ", threshold)
-            print(error)
-            heatmap[i, j] = error
+                    # Calculate error
+                    error = np.linalg.norm(occupations_yaqs[:,-1][::-1] - occupations_qutip[:,-1], 2)
+                    print("max bond dim: ", max_bond_dim, "\t threshold: ", threshold)
+                    print(error)
+                    heatmap[index_dim, index_threshold] = error
 
-    fig, ax = plt.subplots(1, 1)
-    im = ax.imshow(heatmap.T, aspect="auto", norm=LogNorm())
-    title = r"$\Delta t = $" + str(dt) + ", Timesteps = " + str(timesteps) + ", Total simulation time T = " + str(total_time)
-    ax.set_title(title)
-    ax.set_xlabel("max bond dim")
-    ax.set_ylabel("threshold")
-    formatted_threshold_vals = [f"$10^{{{int(np.log10(t))}}}$" for t in threshold_list]
-    tick_positions = np.arange(0, len(max_bond_dim_list), 10)
-    tick_labels = [str(max_bond_dim_list[i]) for i in tick_positions]
-    ax.set_xticks(tick_positions)
-    ax.set_xticklabels(tick_labels)
-    ax.set_yticks(range(len(threshold_list)))
-    ax.set_yticklabels(formatted_threshold_vals)
-    fig.subplots_adjust(top=0.95, right=0.88)
-    cbar_ax = fig.add_axes(rect=(0.9, 0.11, 0.025, 0.8))
-    cbar = fig.colorbar(im, cax=cbar_ax)
-    cbar.ax.set_title("$\\langle Z \\rangle$")
+            im = ax[index_timesteps, index_trotter].imshow(heatmap.T, aspect="auto", norm=LogNorm())
+            title = r"$\Delta t = $" + str(dt) + ", Timesteps = " + str(timesteps) + "\n Total simulation time T = " + str(total_time)
+            ax[index_timesteps, index_trotter].set_title(title, fontsize=8)
+            ax[index_timesteps, index_trotter].set_xlabel("max bond dim")
+            ax[index_timesteps, index_trotter].set_ylabel("threshold")
+            formatted_threshold_vals = [f"$10^{{{int(np.log10(t))}}}$" for t in threshold_list]
+            tick_positions = np.arange(0, len(max_bond_dim_list), 10)
+            tick_labels = [str(max_bond_dim_list[i]) for i in tick_positions]
+            ax[index_timesteps, index_trotter].set_xticks(tick_positions)
+            ax[index_timesteps, index_trotter].set_xticklabels(tick_labels)
+            ax[index_timesteps, index_trotter].set_yticks(range(len(threshold_list)))
+            ax[index_timesteps, index_trotter].set_yticklabels(formatted_threshold_vals)
+            #fig.subplots_adjust(top=0.95, right=0.88)
+            #cbar_ax = fig.add_axes(rect=(0.9, 0.11, 0.025, 0.8))
+            cbar = fig.colorbar(im, ax=ax[index_timesteps, index_trotter])
+            #cbar.ax.set_title("$\\langle Z \\rangle$")
 
+    plt.tight_layout()
     plt.show()
 
 
