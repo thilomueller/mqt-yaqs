ff2268281c274e42c4cddc0a6008b5b17dd5080a
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-04-07 14:48:31 +0200

    threshold changed to match qiskit

--- src/mqt/yaqs/core/methods/tdvp.py
+++ src/mqt/yaqs/core/methods/tdvp.py
@@ -87,7 +87,7 @@ def split_mps_tensor(
     u_mat, sigma, v_mat = np.linalg.svd(matrix_for_svd, full_matrices=False)
 
     cut_sum = 0
-    thresh_sq = sim_params.threshold**2
+    thresh_sq = sim_params.threshold
     cut_index = 1
     for i, s_val in enumerate(np.flip(sigma)):
         cut_sum += s_val**2
@@ -99,12 +99,36 @@ def split_mps_tensor(
     left_tensor = u_mat[:, :cut_index]
     sigma = sigma[:cut_index]
     right_tensor = v_mat[:cut_index, :]
+    norm = np.linalg.norm(sigma)
+    if norm > 0:
+        sigma = sigma / norm
 
     # Reshape U and Vh back to tensor form:
     # U to shape (d0, D0, num_sv)
     left_tensor = left_tensor.reshape((shape_transposed[0], shape_transposed[1], cut_index))
     # Vh reshaped to (num_sv, d1, D2)
     right_tensor = right_tensor.reshape((cut_index, shape_transposed[2], shape_transposed[3]))
+
+    # u_mat, sigma, v_mat = np.linalg.svd(matrix_for_svd, full_matrices=False)
+ 
+    # # Truncate singular values below the threshold
+    # num_sv: int = np.sum(sigma**2 > sim_params.threshold)
+    # if sim_params.max_bond_dim is not None:
+    #     num_sv = min(num_sv, sim_params.max_bond_dim)
+    # sigma = sigma[0:num_sv]
+    # norm = np.linalg.norm(sigma)
+    # if norm > 0:
+    #     sigma = sigma / norm
+
+    # # Truncate U and Vh accordingly
+    # u_mat = u_mat[:, :num_sv]
+    # v_mat = v_mat[:num_sv, :]
+
+    # # Reshape U and Vh back to tensor form:
+    # # U to shape (d0, D0, num_sv)
+    # left_tensor = u_mat.reshape((shape_transposed[0], shape_transposed[1], num_sv))
+    # # Vh reshaped to (num_sv, d1, D2)
+    # right_tensor = v_mat.reshape((num_sv, shape_transposed[2], shape_transposed[3]))
 
     # Distribute the singular values according to the chosen option
     if svd_distribution == "left":
