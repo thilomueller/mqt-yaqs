964fff45317e7e2c5018e5548d42def3d7528bee
thilomueller <thilo.r.mueller@gmail.com>
2025-05-06 17:16:08 +0100

    fixed identitation and pointless comparisons in test cases

--- tests/core/libraries/test_circuit_library.py
+++ tests/core/libraries/test_circuit_library.py
@@ -367,8 +367,8 @@ def test_create_1d_fermi_hubbard_circuit() -> None:
     # are present for every timestep and trotter step (two per trotter step)
     assert op_names.count("cp") == total_qubits
     # Check that the rotation gates from the hopping terms are present
-    op_names.count("rxx") == 2 * (num_sites - 1)
-    op_names.count("ryy") == 2 * (num_sites - 1)
+    assert op_names.count("rxx") == 2 * (num_sites - 1)
+    assert op_names.count("ryy") == 2 * (num_sites - 1)
 
 
 def test_nearest_neighbour_random_circuit_structure() -> None:
