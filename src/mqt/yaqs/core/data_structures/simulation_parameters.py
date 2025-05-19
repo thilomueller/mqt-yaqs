2e31936bd60c5758ad6bb05685d7a197df78fb0e
Aaron Sander <61705296+aaronleesander@users.noreply.github.com>
2025-05-19 15:09:30 +0200

    updated to allow sites as list or int

--- src/mqt/yaqs/core/data_structures/simulation_parameters.py
+++ src/mqt/yaqs/core/data_structures/simulation_parameters.py
@@ -60,7 +60,7 @@ class Observable:
         Initializes the results and trajectories arrays based on the type of simulation parameters provided.
     """
 
-    def __init__(self, gate: BaseGate, sites: list[int]) -> None:
+    def __init__(self, gate: BaseGate, sites: int | list[int]) -> None:
         """Initializes an Observable instance.
 
         Parameters
@@ -186,7 +186,7 @@ class PhysicsSimParams:
             If True, output MPS is returned.
         """
         self.observables = observables
-        self.sorted_observables = sorted(observables, key=lambda obs: (obs.sites[0]))
+        self.sorted_observables = sorted(observables, key=lambda obs: obs.sites[0] if isinstance(obs.sites, list) else obs.sites)
         self.elapsed_time = elapsed_time
         self.dt = dt
         self.times = np.arange(0, elapsed_time + dt, dt)
@@ -371,7 +371,7 @@ class StrongSimParams:
             If True, output MPS is returned.
         """
         self.observables = observables
-        self.sorted_observables = sorted(observables, key=lambda obs: (obs.sites[0]))
+        self.sorted_observables = sorted(observables, key=lambda obs: obs.sites[0] if isinstance(obs.sites, list) else obs.sites)
         self.num_traj = num_traj
         self.max_bond_dim = max_bond_dim
         self.threshold = threshold
