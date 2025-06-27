3375b438e51a3c3137e74f15f58dfd9f0f925442
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-06-27 10:29:29 +0200

    ⚡ Speed up test suite (#103)

--- noxfile.py
+++ noxfile.py
@@ -46,6 +46,11 @@ def _run_tests(
     run_args: Sequence[str] = (),
 ) -> None:
     env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
+
+    if "--cov" in session.posargs:
+        # try to use the lighter-weight `sys.monitoring` coverage core
+        env["COVERAGE_CORE"] = "sysmon"
+
     session.run(
         "uv",
         "run",
