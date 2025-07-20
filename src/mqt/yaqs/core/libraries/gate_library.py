f1fc4d5eb55d7ec97df7d78f1db171ff18a2d690
Alejandro Ramos <aleramos119@gmail.com>
2025-07-20 21:02:19 +0200

    Adds a default "custom" name for gates

--- src/mqt/yaqs/core/libraries/gate_library.py
+++ src/mqt/yaqs/core/libraries/gate_library.py
@@ -131,7 +131,7 @@ class BaseGate:
             Sets the sites on which the gate acts.
     """
 
-    name: str
+    name: str = "custom"
     matrix: NDArray[np.complex128]
     interaction: int
     tensor: NDArray[np.complex128]
@@ -151,7 +151,7 @@ class BaseGate:
         if mat.shape[0] != mat.shape[1]:
             msg = "Matrix must be square"
             raise ValueError(msg)
-
+        
         log = np.log2(mat.shape[0])
 
         self.matrix = mat
@@ -1504,3 +1504,4 @@ class GateLibrary:
     p0 = P0
     p1 = P1
     pvm = PVM
+    custom = BaseGate
